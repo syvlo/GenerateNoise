0adf031ea2676d968500a9c170ad00e947ca3e9b
Sylvain Lobry <sylvainlobry@gmail.com>
2014-02-18 14:56:46 +0100

    Added a function to generate gaussian noise.
          * src/main.cc: Here.
          * CMakeLists.txt: Added support for cxx11.

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,69 @@
+#include <iostream>
+#include <chrono>
+#include <random>
 
+#include <cstring>
+#include <cstdlib>
+
+#include <cv.h>
+#include <highgui.h>
+
+
+void
+printHelp()
+{
+    std::cout << "Generate noise on images." << std::endl
+	      << "Usage:" << std::endl
+	      << "./GenerateNoise gaussian stddev input output" << std::endl;
+}
+
+cv::Mat
+gaussian(double stddev, const cv::Mat& input)
+{
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine generator (seed);
+
+    std::normal_distribution<double> distribution (0.0,stddev);
+
+    unsigned width = input.size().width;
+    unsigned height = input.size().height;
+
+    cv::Mat output(input.size(), CV_8U);
+    for (unsigned i = 0; i < height; ++i)
+	for (unsigned j = 0; j < width; ++j)
+	{
+	    double value = input.at<unsigned char>(i, j) + distribution(generator);
+	    output.at<unsigned char>(i, j) = ((value > 255 ? 255 : value) < 0 ? 0 : value);
+	}
+
+    return output;
+}
 
 int main (int argc, char* argv[])
 {
-    return 0;
+    if (argc <= 1)
+    {
+	printHelp();
+	return (1);
+    }
+
+    else if (!strcmp(argv[1], "gaussian"))
+    { //Add gaussian noise
+	if (argc != 5)//Wrong number of arguments.
+	{
+	    printHelp();
+	    return (1);
+	}
+	double stddev = atof(argv[2]);
+	cv::Mat input = cv::imread(argv[3], CV_LOAD_IMAGE_GRAYSCALE);
+	cv::Mat output = gaussian(stddev, input);
+	cv::imwrite(argv[4], output);
+	return (0);
+    }
+
+    else
+    {
+	printHelp();
+	return (1);
+    }
 }
