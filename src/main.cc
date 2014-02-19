8486ca380120ce1a4e9ca543371c6d45bfb6ede5
Sylvain Lobry <sylvainlobry@gmail.com>
2014-02-19 13:10:51 +0100

    Now check the noise generated (give mean, stddev and coeff of variation).
        * src/main.cc: Here.

--- src/main.cc
+++ src/main.cc
@@ -18,8 +18,39 @@ printHelp()
 	      << "./GenerateNoise rayleigh stddev input output" << std::endl;
 }
 
+void
+checkDistrib(const cv::Mat noise)
+{
+    unsigned width = noise.size().width;
+    unsigned height = noise.size().height;
+    double nbPix = width * height;
+
+    double mean = 0;
+
+    for (unsigned i = 0; i < height; ++i)
+	for (unsigned j = 0; j < width; ++j)
+	{
+	    mean += noise.at<double>(i, j);
+	}
+    mean /= nbPix;
+
+    std::cout << "Mean = " << mean << std::endl;
+
+    double std = 0;
+    for (unsigned i = 0; i < height; ++i)
+	for (unsigned j = 0; j < width; ++j)
+	{
+	    std += (noise.at<double>(i, j) - mean) * (noise.at<double>(i, j) - mean);
+	}
+    std /= nbPix;
+    std = sqrt(std);
+
+    std::cout << "Standart deviation = " << std << std::endl;
+    std::cout << "Coefficient of variation = " << std / mean << std::endl;
+}
+
 cv::Mat
-gaussian(double stddev, const cv::Mat input)
+gaussian(double stddev, const cv::Mat input, bool check)
 {
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine generator (seed);
@@ -30,18 +61,25 @@ gaussian(double stddev, const cv::Mat input)
     unsigned height = input.size().height;
 
     cv::Mat output(input.size(), CV_8U);
+    cv::Mat noise(input.size(), CV_32F);
     for (unsigned i = 0; i < height; ++i)
 	for (unsigned j = 0; j < width; ++j)
 	{
-	    double value = input.at<unsigned char>(i, j) + distribution(generator);
+	    double noiseValue = distribution(generator);
+	    if (check)
+		noise.at<double>(i, j) = noiseValue;
+	    double value = input.at<unsigned char>(i, j) + noiseValue;
 	    output.at<unsigned char>(i, j) = ((value > 255 ? 255 : value) < 0 ? 0 : value);
 	}
+
+    if (check)
+	checkDistrib(noise);
 
     return output;
 }
 
 cv::Mat
-rayleigh(double sigma, const cv::Mat input)
+rayleigh(double sigma, const cv::Mat input, bool check)
 {
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine generator (seed);
@@ -51,13 +89,19 @@ rayleigh(double sigma, const cv::Mat input)
     unsigned height = input.size().height;
 
     cv::Mat output(input.size(), CV_8U);
+    cv::Mat noise(input.size(), CV_32F);
     for (unsigned i = 0; i < height; ++i)
 	for (unsigned j = 0; j < width; ++j)
 	{
 	    double noiseValue = sigma * sqrt(-2 * log(distribution(generator)));
+	    if (check)
+		noise.at<double>(i, j) = noiseValue;
 	    double value = (double)input.at<unsigned char>(i, j) * noiseValue;
 	    output.at<unsigned char>(i, j) = ((value > 255 ? 255 : value) < 0 ? 0 : value);
 	}
+
+    if (check)
+	checkDistrib(noise);
 
     return output;
 }
@@ -79,7 +123,7 @@ int main (int argc, char* argv[])
 	}
 	double stddev = atof(argv[2]);
 	cv::Mat input = cv::imread(argv[3], CV_LOAD_IMAGE_GRAYSCALE);
-	cv::Mat output = gaussian(stddev, input);
+	cv::Mat output = gaussian(stddev, input, true);
 	cv::imwrite(argv[4], output);
 	return (0);
     }
@@ -93,7 +137,7 @@ int main (int argc, char* argv[])
 	}
 	double stddev = atof(argv[2]);
 	cv::Mat input = cv::imread(argv[3], CV_LOAD_IMAGE_GRAYSCALE);
-	cv::Mat output = rayleigh(stddev, input);
+	cv::Mat output = rayleigh(stddev, input, true);
 	cv::imwrite(argv[4], output);
 	return (0);
     }
